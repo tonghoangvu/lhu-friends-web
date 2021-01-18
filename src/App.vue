5ecebdb7b4ab4224274b98f8e23ed454cc7f00fb
tonghoangvu <tonghoangvu@outlook.com>
2021-01-18 21:16:11 +0700

    Design giao diện tổng thể trang web

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
-    <nav>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <main class="flex-col">
+        <nav class="flex-row flex-wrap bg-0">
+            <div class="flex-row m05">
+                <router-link class="tab px1" to="/">Home</router-link>
+                <router-link class="tab px1" to="/about">About</router-link>
+            </div>
+            <div class="flex-row align-center m05 mla">
+                <span class="spinner"></span>
+                <button class="m05 ml1 no-wrap">Đăng nhập</button>
+            </div>
+        </nav>
+        <div class="flex-autosize m05">
+            <router-view/>
+        </div>
+    </main>
 </template>
 
 <script lang="ts">
@@ -15,6 +25,21 @@
     });
 </script>
 
-<style>
+<style scoped>
+    main {
+        height: 100vh;
+        min-height: 100vh;
+    }
 
+    .tab {
+        display: flex;
+        align-items: center;
+        color: var(--text-color);
+        border-bottom: var(--bold-thickness) solid transparent;
+    }
+
+    .tab.router-link-active {
+        color: var(--primary-color);
+        border-bottom: var(--bold-thickness) solid var(--primary-color);
+    }
 </style>
