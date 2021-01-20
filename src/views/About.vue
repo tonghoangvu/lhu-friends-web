70d21d8fdf10c28209c1cb140cbeb2a2e56ebbbf
tonghoangvu <tonghoangvu@outlook.com>
2021-01-20 21:04:40 +0700

    Update logo mới cho web

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,18 +1,16 @@
 <template>
     <div class="flex-col my2 flex-center no-select">
-        <img src="/icons/icon-96.png" alt="">
+        <img src="/img/icon.svg" width="96" alt="">
         <h2 class="text-primary m0">lhu-friends</h2>
         <p class="text-gray">Tiện ích tìm kiếm bạn bè LHU</p>
-        <p>
+        <p class="text-center">
             <a href="https://github.com/tonghoangvu/lhu-friends-web"
                 target="_blank" rel="noreferrer">
                 GitHub project
             </a>
             Made with <span class="text-red">&#x2764;</span>
             <br>
-            <a target="_blank" href="https://icons8.com/icons/set/meeting">Meeting icon</a>
-            icon by
-            <a target="_blank" href="https://icons8.com">Icons8</a>
+            <a href="https://icons8.com/icon/BKCQPf8biWWs/about">About icon by Icons8</a>
         </p>
     </div>
 </template>
