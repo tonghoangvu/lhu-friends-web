6bab05217153961eda316bad1ea5e15dc46960dc
tonghoangvu <tonghoangvu@outlook.com>
2021-01-26 23:03:25 +0700

    Cấu hình PWA chi tiết hơn

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,16 @@
+/* eslint-disable camelcase */
+/* eslint-disable @typescript-eslint/camelcase */
+
 module.exports = {
     lintOnSave: false,
     pwa: {
-        name: 'lhu-friends',
+        name: 'LHU friends',
         themeColor: '#0d6efd',
-        msTileColor: '#0d6efd'
+        msTileColor: '#0d6efd',
+        manifestOptions: {
+            short_name: 'lhu-friends',
+            start_url: '/',
+            background_color: '#fafafa',
+        }
     }
 };
