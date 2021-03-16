9591bfc6b2fc28f53b6a145598d2537b375b46cc
tonghoangvu <tonghoangvu@outlook.com>
2021-03-16 17:19:16 +0700

    Cập nhật ESLint rule mới

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,60 +1,70 @@
 module.exports = {
-    'root': true,
-    'env': {
-        'browser': true,
-        'node': 'true',
-        'es6': true
+    root: true,
+    env: {
+        // String values
+        browser: 'true',
+        node: 'false',
+        es6: 'true'
     },
-    'extends': [
+    extends: [
         'plugin:vue/vue3-essential',
         'eslint:recommended',
         '@vue/typescript/recommended'
     ],
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
     },
-    'parserOptions': {
-        'ecmaVersion': 2020,
-        'sourceType': 'module'
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module'
     },
-    'plugins': ['vue'],
-    'rules': {
+    plugins: ['vue'],
+    rules: {
+        'no-var': ['warn'],
+        'no-use-before-define': ['error'],
+        'no-trailing-spaces': ['warn'],
+        'no-multiple-empty-lines': ['warn'],
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'max-len': ['warn',
-            { 'code': 100, 'ignoreUrls': true }
-        ],
         'vue/script-indent': ['warn', 4,
-            { 'baseIndent': 1 }
+            {
+                baseIndent: 1
+            }
         ],
         'indent': ['warn', 4,
             {
-                'SwitchCase': 1,
-                'FunctionDeclaration': {
-                    'body': 1,
-                    'parameters': 2
+                SwitchCase: 1,
+                FunctionDeclaration: {
+                    body: 1,
+                    parameters: 2
                 }
+            }
+        ],
+        'max-len': ['warn',
+            {
+                code: 100,
+                ignoreUrls: true
             }
         ],
         'linebreak-style': ['warn', 'unix'],
         'quotes': ['warn', 'single'],
         'semi': ['warn', 'always'],
-        'no-var': ['warn'],
-        'no-trailing-spaces': ['warn'],
         'comma-dangle': ['warn', 'never'],
         'eol-last': ['warn', 'always'],
-        'brace-style': ['warn', '1tbs', { 'allowSingleLine': true }],
+        'brace-style': ['warn', '1tbs',
+            {
+                allowSingleLine: true
+            }
+        ],
         'prefer-const': ['warn'],
-        'no-multiple-empty-lines': ['warn'],
-        'space-in-parens': ['warn'],
-        'no-use-before-define': ['error']
+        'space-in-parens': ['warn']
     },
-    'overrides': [
+    overrides: [
         {
-            'files': ['*.vue'],
-            'rules': {
-                'indent': 'off'
+            files: ['*.vue'],
+            rules: {
+                indent: 'off'
             }
         }
     ]
