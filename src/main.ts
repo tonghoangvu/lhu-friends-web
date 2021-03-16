bfbc084f7beb86c6b9b57f3aca13c5d5c2d10efa
tonghoangvu <tonghoangvu@outlook.com>
2021-03-16 17:39:09 +0700

    Đổi cách import CSS

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,14 @@ import App from '@/App.vue';
 import router from '@/router';
 import store from '@/store';
 
+import '@/assets/your-css/colors.css';
+import '@/assets/your-css/default.css';
+import '@/assets/your-css/default-fix.css';
+import '@/assets/your-css/classes.css';
+import '@/assets/your-css/sizes.css';
+import '@/assets/your-css/flex.css';
+import '@/assets/your-css/scrollbar.css';
+import '@/assets/your-css/components.css';
+import '@/assets/your-css/utils.css';
+
 createApp(App).use(store).use(router).mount('#app');
